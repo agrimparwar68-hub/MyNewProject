0010f6d4fd991f2c8a5e83b2113df25b059073d2
AMD <agrimparwar68@gmail.com>
2026-05-17 17:40:26 +0530

    C5-number of vowels code added

--- user_length.py
+++ user_length.py
@@ -1,6 +1,14 @@
-# print length of given user name 
+# print length of given user name and print number of vowels
 def user_name():
-    name = str(input("Enter your name:- "))
-    print(len(name))
+    name = input("Enter your name:- ")
+    print("Length of name:", len(name))
+    
+    vowels = "aeiouAEIOU"
+    count = 0
+    for ch in name:
+        if ch in vowels:
+            count += 1
+    
+    print("Number of vowels:", count)
 
 user_name()
